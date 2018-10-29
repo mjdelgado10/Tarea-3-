6c5f44c1e4e108f7e34bf7b79c05d84e08efda3d
mjdelgado10 <42391158+mjdelgado10@users.noreply.github.com>
2018-10-29 16:32:44 -0500

    Add files via upload
    En esta parte del primer punto se inicia el ciclo que recorrera las variables para crear la covarianza. se genera el promedio de los datos y luego se genera la covarianza, luego se calcula cada fila de la matriz y se crea el array de covarianza, al final, imprime covarianza

--- DM_PCR.py
+++ DM_PCR.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 #Regresion PCR
@@ -21,7 +21,27 @@ variables = np.genfromtxt('WDBC.dat', usecols=columnas, delimiter=',')
 n,m = np.shape(variables)
 
 #Inicializacion para empezar la covarianza
-Covarianza = [] 
+Covarianza = []
+#Inicio del ciclo que recorrera todos los datos
+for i in range(m):
+    fila_matriz = [] # lista vacia
+#Ciclo que genera el promedio de i y j de la variable m
+    for j in range(m):
+        promedio_i = np.mean(variables[:,i])
+        promedio_j = np.mean(variables[:,j])
+#genera la covarianza de las variables segun la formula. 
+        Covar = np.sum( (variables[:,i]-promedio_i) * (variables[:,j]-promedio_j) )/(n-1)
+# Calcula cada fila de la matriz y la guarda
+        fila_matriz.append(Covar)
+    Covarianza.append(fila_matriz)
+#Una vez se llena la lista, se crea el array
+Covarianza = np.array(Covarianza)
+
+#Imprime el valor de la covarianza
+
+print(Covarianza, '\n\n', np.cov(variables.T))
+
+
 
 
 # In[ ]:
