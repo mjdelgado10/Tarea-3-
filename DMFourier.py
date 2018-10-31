279addec4a6d1de2aa2c4ddc0bddba5a052f57ea
mjdelgado10 <42391158+mjdelgado10@users.noreply.github.com>
2018-10-30 23:00:06 -0700

    Add files via upload
    En este avance se generó la transformada de cada una de las señales  y ademas de no usar np.fft.fftfreq que calcula la transformada de fourier discreta, se crea la grafica de las frecuencias  y se imprime los mayores valores de las mismas.

--- DMFourier.py
+++ DMFourier.py
@@ -6,6 +6,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fftpack import ifft
+from scipy.interpolate import interp1d
 archivo1 = 'signal.dat'
 archivo2 = 'incompletos.dat'
 delimitador = ','
@@ -45,8 +47,7 @@ def mi_fourier(senal):
     transformada_r = np.zeros(N) 
     transformada_i = np.zeros(N) 
     
-
-	# vector con los valores de N
+# vector con los valores de N
     n = np.linspace(0, N-1, N) 
 #Ciclo que calcula la parte real y imaginaria de la tranformada
     for k in range(N):
@@ -61,7 +62,7 @@ def mi_fourier(senal):
 # delta de la frecuencia
     D = 1.0/(2.0*(x[-1]-x[-2]))*(1/(N/2)) 
     
-   
+    
     
 #Ciclo que genera las frecuencias a pasos de delta de las mismas. 
     for i in range(int(N/2)):
@@ -69,8 +70,47 @@ def mi_fourier(senal):
 	#valores al reves 
     for i in range(-1,-int(N/2)-1, -1):
         frecuencias[i] = (i)*D
-    #rretorna los valores de la frecuencia y de la transformada real y imaginaria.
+    #retorna los valores de la frecuencia y de la transformada real y imaginaria.
     return( frecuencias, transformada_r, transformada_i)
+
+
+# Calcular fourier de la señal 1 llamando a la funcion mi fourier
+f1, tf_r1, tf_i1 = mi_fourier(senal1) 
+#calcula la amplitud de la primera señal
+amplitud1 = (tf_r1**2 + tf_i1**2)**0.5 # calcular la amplitud
+
+# Calcular fourier de la señal 2 llamando a la funcion mi fourier
+f2, tf_r2, tf_i2 = mi_fourier(senal2) 
+# calcular la amplitud de la segunda señal
+amplitud2 = (tf_r2**2 + tf_i2**2)**0.5 
+
+
+# Calcular fourier de la señal 3 llamando a la funcion mi fourier
+f3, tf_r3, tf_i3 = mi_fourier(senal3) 
+# calcular la amplitud de la tercera señal
+amplitud3 = (tf_r3**2 + tf_i3**2)**0.5 
+
+# Calcular fourier de la señal 4 llamando a la funcion mi fourier
+f4, tf_r4, tf_i4 = mi_fourier(senal4) 
+# calcular la amplitud de la cuarta señal
+amplitud4 = (tf_r4**2 + tf_i4**2)**0.5 
+
+
+#Imprime un mensaje por medio de un "print " para indicar que no se uso la herramienta fftfreq del paquete de Scipy
+print("Mensaje para indicar que no se empleo fftfreq para calcular las frecuencias")
+
+#Genera la grafica de las frecuencias y la guarda en formato PDF
+fig = plt.figure()
+ax = fig.gca()
+ax.plot(f1, amplitud1)
+ax.set_xlabel('Frecuencia')
+ax.set_ylabel('Magnitud')
+ax.grid()
+ax.set_xlim([-2000,2000])
+fig.savefig('DelgadoMaria_TF.pdf', type='pdf')
+
+#Imprime el numero de picos de las frecuencias encontradas
+print("Hay picos de frecuencias en 35, 180, 220 y 410")
 
 
 # In[ ]:
